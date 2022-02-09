9c6e3f7dd4d4e6418568b0e1c4b1c323a3b15ddd
Ryan1Up <anderson94@mail.fresnostate.edu>
2022-02-09 10:18:54 -0800

    Init Server Scaffolding

---
+++ java services/server/server/src/test/java/com/CSCI152/team4/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CSCI152.team4.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
