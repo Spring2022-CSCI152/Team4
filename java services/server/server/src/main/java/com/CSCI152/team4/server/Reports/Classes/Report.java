16e26fa919bba7a7fe539dc8a9ed49e693439c2e
Ryan1Up <anderson94@mail.fresnostate.edu>
2022-04-08 15:48:44 -0700

    Controller and Request Scaffolding

--- java services/server/server/src/main/java/com/CSCI152/team4/server/Reports/Classes/Report.java
+++ java services/server/server/src/main/java/com/CSCI152/team4/server/Reports/Classes/Report.java
@@ -1,4 +1,17 @@
 package com.CSCI152.team4.server.Reports.Classes;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/reports")
 public class Report {
+
+    /*
+    * get and set formats
+    *
+    * get and submit reports
+    *   Upon report submission extract list of profiles attached and/or
+    *   create report submission request that contains a list of profiles
+    * get profiles*/
 }
---
+++ java services/server/server/src/main/java/com/CSCI152/team4/server/Reports/Classes/ReportSubmissionRequest.java
@@ -0,0 +1,7 @@
+package com.CSCI152.team4.server.Reports.Classes;
+
+/*This class will serve as a Wrapper for an incoming
+* report submission. It will house the Report Fields themselves
+* as well as a list of profiles attached to the report*/
+public class ReportSubmissionRequest {
+}
