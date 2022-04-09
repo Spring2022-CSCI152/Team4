b8efae1289d00be594efde493941d8352589ec4a
Ryan1Up <anderson94@mail.fresnostate.edu>
2022-04-08 17:18:40 -0700

    Validator

--- java services/server/server/src/main/java/com/CSCI152/team4/server/Reports/Validator/ReportValidator.java
+++ java services/server/server/src/main/java/com/CSCI152/team4/server/Reports/Validator/ReportValidator.java
@@ -1,6 +1,23 @@
 package com.CSCI152.team4.server.Reports.Validator;
 
+import com.CSCI152.team4.server.Reports.Classes.Profile;
+import com.CSCI152.team4.server.Reports.Classes.Report;
+
+import java.util.List;
+
 /*This class serves as a utility to ensure only the desired input is
 * accepted and saved*/
 public class ReportValidator {
+
+    public void validateReport(Report report){
+
+    }
+
+    public void validateProfile(Profile profile){
+
+    }
+
+    public void validateProfiles(List<Profile> profileList){
+        
+    }
 }
