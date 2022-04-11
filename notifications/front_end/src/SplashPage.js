187aef3a9108582baf4262a43605338a3c58c0f0
MSeechan <66396292+MSeechan@users.noreply.github.com>
2022-04-10 18:47:26 -0700

    working on add report

--- notifications/front_end/src/SplashPage.js
+++ notifications/front_end/src/SplashPage.js
@@ -3,6 +3,7 @@ import React from "react";
 import SignIn from "./components/SignIn";
 import Register from "./components/Register";
 import { useState } from "react";
+import { Navbar } from "react-bootstrap";
 
 const SplashPage = ({ authenticate }) => {
 
@@ -16,14 +17,14 @@ const SplashPage = ({ authenticate }) => {
   
   return (
     <div className="rows-2 justify-content-center">
-      <div className="txt-align-center">
+      <div className="txt-align-center mt-5">
         <img src={logo} />
         <br />
-        <h1 className="fw-bolder">FR Most Wanted</h1>
+        <h4><b>FR MOST WANTED </b></h4>
       </div>
 
       {/* Toggle sign in and register forms*/}
-      <div className="row txt-align-center ">
+      <div className="row txt-align-center">
         <div className="col"></div>
         <div className="col">
           {" "}
--- notifications/front_end/src/components/AllList.js
+++ notifications/front_end/src/components/AllList.js
@@ -4,43 +4,47 @@ import ReactPaginate from "react-paginate";
 import { FaPen, FaCaretLeft, FaCaretRight } from "react-icons/fa";
 import DashBtns from "./DashBtns";
 import ReportModal from "./ReportModal";
-import { Card, Stack } from "react-bootstrap";
+import { Card, Stack, nav } from "react-bootstrap";
 
 function AllList() {
-  {/* Cards display and page calculation */}
+  {
+    /* Cards display and page calculation */
+  }
 
   const [profiles, setProfiles] = useState(mockData);
   const [pageNum, setPageNum] = useState(0);
-  const cardsPerPage = 6;
+  const cardsPerPage = 2;
   const pagesVisited = pageNum * cardsPerPage;
   const AllReports = profiles
     .slice(pagesVisited, pagesVisited + cardsPerPage)
     .map((profiles) => {
       return (
-        <Card className="card-margin" key={profiles.r_id} onClick={() => setIsOpen(true)}>
+        <Card
+          className="card-margin"
+          key={profiles.r_id}
+          onClick={() => setIsOpen(true)}
+        >
           <div className="row p-1 d-flex align-items-center">
-            <div className="col-1 p-1">
+            <div className="col-auto p-1">
               <p>ID: {profiles.r_id}</p>
               <p>{profiles.date}</p>
             </div>
 
             <div className="col-5 p-1">
-              <div className="col p-1">
-                <div className="col-12  p-1">
-                  {profiles.profile.map((item, i) => {
-                    return (
-                      <div key={i} className="d-flex justify-content-end">
-                        <img src={item.url} />
-                      </div>
-                    );
-                  })}
-                </div>
-              </div>
-
-              <div className="col-12 p-1 d-flex align-content-around flex-wrap">
+              <div className="col-12">
                 {profiles.profile.map((item, i) => {
                   return (
-                    <div key={i}>
+                    <div key={i} className="d-flex justify-content-end">
+                      <img src={item.url} />
+                    </div>
+                  );
+                })}
+              </div>
+
+              <div className="d-flex align-content-around flex-wrap">
+                {profiles.profile.map((item, i) => {
+                  return (
+                    <div className="col-auto" key={i}>
                       <Stack>
                         {item.name}
                         <br />
@@ -56,14 +60,12 @@ function AllList() {
             <div className="col-2 p-1"> {profiles.location} </div>
             <div className="col-1 p-1"> {profiles.attachements}</div>
             <div className="col-1 p-1">
-              <button className="btn sm round">
-                {""}edit <FaPen size="1em" />
-                {""}
+              <button type="button" className="btn btn-sm btn-light mr-5">
+                Edit <FaPen size=".75em" />
               </button>
             </div>
           </div>
         </Card>
-
       );
     });
 
@@ -76,13 +78,12 @@ function AllList() {
   return (
     <main>
       <ReportModal open={isOpen} onClose={() => setIsOpen(false)}>
-        {" "}
       </ReportModal>
       {<DashBtns />}
       <div className="searchBar">search</div>
-      <div className="card-label txt-align-center card-margin">
+      <div className="card-label txt-align-left card-margin">
         <div>Report Number</div>
-        <div className="txt-align-left ">Involved Parties</div>
+        <div>Involved Parties</div>
         <div>Type</div>
         <div>Location</div>
         <div>Attachments</div>
