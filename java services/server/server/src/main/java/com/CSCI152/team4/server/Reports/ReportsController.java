df69e6ebbfb9dd70df6444b623bb5b0b18c4469a
Ryan1Up <anderson94@mail.fresnostate.edu>
2022-04-17 09:21:38 -0700

    Add: Reports Controller Get and Set Formats

--- java services/server/server/src/main/java/com/CSCI152/team4/server/Reports/ReportsController.java
+++ java services/server/server/src/main/java/com/CSCI152/team4/server/Reports/ReportsController.java
@@ -1,15 +1,16 @@
 package com.CSCI152.team4.server.Reports;
 
+import com.CSCI152.team4.server.Accounts.Settings.CustomerProfileFormat;
+import com.CSCI152.team4.server.Accounts.Settings.ReportFormat;
+import com.CSCI152.team4.server.Reports.Requests.ProfileFormatUpdateRequest;
 import com.CSCI152.team4.server.Reports.Requests.ReportFormatUpdateRequest;
 import com.CSCI152.team4.server.Reports.Service.ReportsService;
 import com.CSCI152.team4.server.Reports.Service.SettingsService;
+import com.CSCI152.team4.server.Util.InstanceClasses.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/reports")
@@ -27,7 +28,27 @@ public class ReportsController {
     @PostMapping("/set_report_format")
     public ResponseEntity<Enum<HttpStatus>> setReportFormat(@RequestBody ReportFormatUpdateRequest request){
 
+        settingsService.setReportFormat(request);
 
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+    @PostMapping("/set_profile_format")
+    public ResponseEntity<Enum<HttpStatus>> setReportFormat(@RequestBody ProfileFormatUpdateRequest request){
+
+        settingsService.setProfileFormat(request);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/get_report_format")
+    public ReportFormat getReportFormat(@RequestBody Request request){
+        return settingsService.getReportFormat(request);
+    }
+
+    @GetMapping("/get_profile_format")
+    public CustomerProfileFormat getProfileFormat(@RequestBody Request request){
+        return settingsService.getProfileFormat(request);
     }
 }
