65f74b46202f457f905efb60c056cad2d3b6f40e
MSeechan <66396292+MSeechan@users.noreply.github.com>
2022-04-18 16:16:51 -0700

    adding image upload to add report

--- notifications/front_end/src/components/AddReport.js
+++ notifications/front_end/src/components/AddReport.js
@@ -1,10 +1,19 @@
-import { InputGroup, FormControl, DropdownButton, Dropdown } from "react-bootstrap";
+import US_states from "./US-states";
+import mockData from "../mockData";
+import { Button } from "react-bootstrap";
 
 const AddReport = () => {
+
+    const uploadImage = (files) => {
+      const formData = new FormData()
+      formData.append("img", files[0])
+      console.log(formData)
+    }
+
     return (
-      <form className="row align-bottom">
+      <form className="row m-2">
         <h3>New Incident</h3>
-        <div className="col-2">
+        <div className="col-2 mb-3">
           <label htmlFor="inputDate" className="form-label">Incident Date</label>
           <input type="date" className="form-control" id="inputDate"/>
         </div>
@@ -12,51 +21,75 @@ const AddReport = () => {
           <label  htmlFor="inputTime" className="form-label">Time</label>
           <input type="time" className="form-control" id="inputTime"/>
         </div>
-
-
-        <select className="w-auto form-select-sm col-2" aria-label="Default select example">
-          <option defaultValue={"..."}>Incident Type</option>
-          <option value="1">Assault</option>
-          <option value="2">Verbal</option>
-          <option value="3">something else</option>
+      <div className="col-2">
+        <label  htmlFor="inputType" className="form-label">Location</label>
+        <input type="text" className="form-control" id="inputType"/>
+      </div>
+      <div className="col-2">    
+        <label  htmlFor="inputTime" className="form-label">Incident Type</label>
+        <select className="w-auto form-select-sm col-2 dropdown" aria-label="Default select example">
+          <option defaultValue={""}>Verbal</option>
+          <option value="1">Physical</option>
+          <option value="2">Warning</option>
         </select>
+      </div>
+      <hr className="green"></hr>
+      <h6>Involved Parties</h6>
+      <div className="col-7 mb-3">     
+          <label  htmlFor="inputProfile" className="form-label">Existing Profiles</label>
+          <select className="w-auto form-select-sm col-2 dropdown" aria-label="Default select example">
+            <option defaultValue={"..."}></option>
 
+            {mockData.profile.map((item, i) => {
+                  return (
+                    <div key={i} className="d-flex justify-content-end">
+                      <img src={item.url} />
+                    </div>
+                  );
+                })}
+          
+            
 
-
-        <div className="col-2">
-          <label  htmlFor="inputType" className="form-label">Type</label>
-          <input type="text" className="form-control" id="inputType"/>
+          </select>
         </div>
-        <div className="col-12">
-          <label  htmlFor="inputAddress2" className="form-label">Address 2</label>
-          <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"/>
+        <div className="col-md-5 mb-3">
+          <label  htmlFor="inputAddress2" className="form-label">Address</label>
+          <input type="text" className="form-control" id="inputAddress2"/>
         </div>
-        <div className="col-md-6">
+        <div className="col-md-3">
           <label  htmlFor="inputCity" className="form-label">City</label>
           <input type="text" className="form-control" id="inputCity"/>
         </div>
-        <div className="col-md-4">
+        <div className="col-md-1">
           <label  htmlFor="inputState" className="form-label">State</label>
           <select id="inputState" className="form-select">
-            <option selected>Choose...</option>
             <option>...</option>
+            {mockData.map( (item,i)=>{return( <option key={i}> {item} </option> )})}
           </select>
         </div>
         <div className="col-md-2">
           <label  htmlFor="inputZip" className="form-label">Zip</label>
           <input type="text" className="form-control" id="inputZip"/>
+        </div> 
+
+        <div> <input type ="file" 
+          onChange ={
+            (event)=>{ uploadImage(event.target.files);}
+            }/>
+        </div>
+
+     
+        <div className="col-12">
+  
         </div>
         <div className="col-12">
-          <div className="form-check">
-            <input className="form-check-input" type="checkbox" id="gridCheck"/>
-            <label className="form-check-label" for="gridCheck">
-              Check me out
-            </label>
-          </div>
+          <button type="submit" className="btn green btn-sm">Add Person</button>     
+          <hr className="green"></hr>
         </div>
-        <div className="col-12">
-          <button type="submit" className="btn green">Submit</button>
-        </div>
+
+
+   
+
       </form>
     );
   };
