a9e5e2f847be295db09edc1f5f4b998d2fd69783
Ryan1Up <anderson94@mail.fresnostate.edu>
2022-04-25 10:22:58 -0700

    Skillz

--- java services/server/server/src/main/java/com/CSCI152/team4/server/Accounts/Services/AccountManagementService.java
+++ java services/server/server/src/main/java/com/CSCI152/team4/server/Accounts/Services/AccountManagementService.java
@@ -279,15 +279,20 @@ public class AccountManagementService {
     private WorkerAccount transposeTo(Class accountType, WorkerAccount account)
             throws InvocationTargetException, InstantiationException, IllegalAccessException {
         Constructor con = null;
+
+        /*Iterate throw class constructors*/
         for(Constructor c : accountType.getDeclaredConstructors()){
             if(c.getParameterCount() == 6){
                 con = c;
             }
         }
+        /*Create new instance using 6 param constructor*/
         WorkerAccount ret =  (WorkerAccount) con.newInstance(account.getBusinessId(),
                 account.getEmail(), account.getPassword(),
                 account.getFirstName(), account.getLastName(),
                 account.getJobTitle());
+        
+        /*Set account id to original ID*/
         ret.setAccountId(account.getAccountIdString());
         return ret;
     }
