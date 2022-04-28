673c95a1caff49b3179d00c2ed09f17d42cab2b2
MSeechan <66396292+MSeechan@users.noreply.github.com>
2022-04-28 11:26:16 -0700

    testing requests with postman

--- notifications/server/expressServer.js
+++ notifications/server/expressServer.js
@@ -2,16 +2,21 @@ const express = require("express")
 const app = express()
 const port = 4000
 const cors = require("cors")
+const http = require('http');
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors())
 
-
+app.get('/post_registration_form', (req,res)=>{
+    res.send({msg: 'postman get test', user: {}})
+})
 
 app.post("/post_registration_form", async (req, res) => {
-	let { formData } = req.body
-})
+    console.log('body print here: ', req.body);
+    res.status(200).send('create admin postman test')
+    
+}); 
+
 app.listen(port, () => {
-	console.log(`Listening at http://localhost:${port}`)
-})
+    console.log(`Listening at http://localhost:${port}`)})
