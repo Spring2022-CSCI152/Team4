9293613b311f3ddd4951aa5fc1834d0eb901fce7
Ryan1Up <anderson94@mail.fresnostate.edu>
2022-04-30 02:38:52 -0700

    Test Outline

---
+++ java services/server/server/src/test/java/com/CSCI152/team4/server/Accounts/Utils/AccountPermissionUpdaterTest.java
@@ -0,0 +1,20 @@
+package com.CSCI152.team4.server.Accounts.Utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountPermissionUpdaterTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void itShouldUpdatePermissions() {
+        // Given
+        // When
+        // Then
+    }
+}
---
+++ java services/server/server/src/test/java/com/CSCI152/team4/server/Accounts/Utils/AccountTransposerTest.java
@@ -0,0 +1,15 @@
+package com.CSCI152.team4.server.Accounts.Utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTransposerTest {
+
+    @Test
+    void itShouldTransposeTo() {
+        // Given
+        // When
+        // Then
+    }
+}
