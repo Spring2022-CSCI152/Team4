c72089e5b573a6c2630811ac8ed4d3d9378f6d6e
Ryan1Up <anderson94@mail.fresnostate.edu>
2022-05-03 14:08:47 -0700

    NO TOAST

--- notifications/server/example_notif.jsx
+++ notifications/server/example_notif.jsx
@@ -49,8 +49,6 @@ const Notif = () => {
         else{
             return ( 
                 <div>
-                    <button onClick={notify}>Notify!</button>
-                    <ToastContainer />
                 </div>)
         }
     }
